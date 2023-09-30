e9d07dada8c8ca26613a397065c7b8bc984f44e1
JASPAL SINGH <jpmertiya57@gmail.com>
2023-09-30 20:10:19 +0530

    Initializing Library Management Project

---
+++ LibraryManaggement/src/main/java/com/Library/Management/LibraryManaggementApplication.java
@@ -0,0 +1,13 @@
+package com.Library.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManaggementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryManaggementApplication.class, args);
+	}
+
+}
